a4a64ebe31d6a32ff0a9b004fdce2426468acbea
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-12-19 16:32:33 -0300

    refac: use built-in callback binding

--- bin/read2.ml
+++ bin/read2.ml
@@ -1,8 +1,10 @@
 open Lwt_io
 
 let p =
-  Lwt.bind (read_line stdin) (fun s1 ->
-    Lwt.bind (read_line stdin) (fun s2 ->
-      Lwt_io.printf "%s\n" (s1^s2)))
-      
+  let%lwt s1 = read_line stdin in
+  let%lwt s2 = read_line stdin in
+  Lwt_io.printf "%s\n" (s1^s2)
+
+let _ = Lwt_io.printf "Got here first\n"
+
 let _ = Lwt_main.run p
